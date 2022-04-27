4fcc6f03c16fb958dfb9b9b9f525e4212a90d4f4
rakibul-islam-github <freelancing.everyday@gmail.com>
2022-04-27 15:54:17 +0600

    home hero section added

---
+++ components/HomeHero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HomeHero = () => {
+    return (
+        <>
+            <div className="col-md-6 hero-text">
+                <h1>Discover, Collect & buy your Digital Slot</h1>
+                <p className="mt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum deleniti soluta optio officia architecto corporis quibusdam vitae recusandae est. Delectus!</p>
+
+                <a href="#slot" className="btn-header btn">Explore More</a>
+            </div>
+        </>
+    );
+};
+
+export default HomeHero;
