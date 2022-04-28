88437cfaf88c37685da999786df5018ee4131223
rakibul-islam-github <freelancing.everyday@gmail.com>
2022-04-28 11:33:19 +0600

    index page updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../components/header/header.css'
 import '../styles/globals.css'
+import '../styles/modal.css'
 
 function MyApp({ Component, pageProps }) {
   
