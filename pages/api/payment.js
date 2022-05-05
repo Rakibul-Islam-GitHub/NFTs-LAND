4a816b499a1db244363d94126e193783cfd423e3
rakibul-islam-github <freelancing.everyday@gmail.com>
2022-05-05 22:58:41 +0600

    payment api added

---
+++ pages/api/payment.js
@@ -0,0 +1,53 @@
+import {client} from '../../database/dbConnect';
+
+function handler(req, res) {
+    
+    if (req.method === 'POST') {
+        
+        const {orderID, payerAddress, payerEmail, user, amount, paymentAt} = req.body
+        
+        if(orderID && payerAddress && payerEmail && user && amount && paymentAt) {
+
+            console.log('from api', orderID);
+
+            client.connect(async err => {
+                const collection = client.db("nft-slot").collection("payments");
+               
+                const result= await collection.insertOne(
+                    {
+                        orderID: orderID,
+                        payerAddress: payerAddress,
+                        payerEmail: payerEmail,
+                        user: user,
+                        amount: amount,
+                        paymentAt: paymentAt
+
+                    });
+                
+                if(result){
+                    client.close();
+                    
+                    res.status(200).json({success: true, status:200});
+                }else{
+                    console.log(err);
+                    res.status(500).json({success: false});
+                }
+                
+              });
+
+
+            
+            
+
+        }
+        else{
+            res.status(403).json({message:'Invalid data'})
+        }
+       
+    }else{
+        res.status(404).json({message:'Request not found'})
+    }
+}
+
+
+export default handler;
